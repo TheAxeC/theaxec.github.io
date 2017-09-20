65a633b00be07794372fc43fbfae70a4f2917eba
Axel Faes <axel.faes@gmail.com>
2017-09-20 21:00:07 +0200

    - update scripts

--- _plugins/cat_gen.rb
+++ _plugins/cat_gen.rb
@@ -1,6 +1,6 @@
 module Jekyll
 
-	class CategoryIndex < Page    
+	class CategoryIndex < Page
 		def initialize(site, base, dir, tag)
 			@site = site
 			@base = base
@@ -15,16 +15,20 @@ module Jekyll
 
 	class CategoryGenerator < Generator
 		safe true
-	
+
 		def generate(site)
 			if site.layouts.key? 'cat_index'
 				dir = 'category'
 				site.categories.keys.each do |category|
 					write_cat_index(site, File.join(dir, category), category)
 				end
+				dir = 'blog'
+				site.categories.keys.each do |category|
+					write_cat_index(site, File.join(dir, category), category)
+				end
 			end
 		end
-	
+
 		def write_cat_index(site, dir, category)
 			index = CategoryIndex.new(site, site.source, dir, category)
 			index.render(site.layouts, site.site_payload)
