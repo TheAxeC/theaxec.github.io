ff2ba3053204e61ebbed3b00063d1eeb12005325
TheAxeC <axel.faes@gmail.com>
2025-10-21 22:48:09 +0200

    update current position

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ python _scripts/script.py --save --quiet
 # If the Python script succeeds, start the Jekyll server
 if [ $? -eq 0 ]; then
     echo "Python script completed successfully. Starting Jekyll server..."
-    bundle exec jekyll serve --livereload --incremental --trace
+    bundle exec jekyll serve
 else
     echo "Python script failed. Jekyll server not started."
     exit 1
